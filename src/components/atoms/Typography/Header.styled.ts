d13652233d2d739e7068bd3ee1af67c4a889ea00
CezaryWrzalik <wrzalikc@gmail.com>
2022-04-19 19:31:08 +0200

    (Feat): resize large headers fonts for mobile

--- src/components/atoms/Typography/Header.styled.ts
+++ src/components/atoms/Typography/Header.styled.ts
@@ -1,45 +1,54 @@
-import styled, { css } from 'styled-components';
-import { repeatableStyles } from './repeatableStyles.styled';
+import { breakpoints } from "@shared/constants";
+import styled, { css } from "styled-components";
+import { repeatableStyles } from "./repeatableStyles.styled";
 
 export const repeatableHeaderCss = css(
-    ({ theme: { fontFamily } }) => css`
-        ${repeatableStyles};
-        font-family: ${fontFamily.gtw_pro};
-    `,
+  ({ theme: { fontFamily } }) => css`
+    ${repeatableStyles};
+    font-family: ${fontFamily.gtw_pro};
+  `
 );
 
 export const TextH1_96 = styled.h1(
-	({ theme: { fontSizes } }) => css`
-			${repeatableHeaderCss};
-			font-size: ${fontSizes[96]}px;
-	`,
+  ({ theme: { fontSizes } }) => css`
+    ${repeatableHeaderCss};
+    font-size: ${fontSizes[96]}px;
+
+    @media (max-width: ${breakpoints.sm}px), (max-height: ${breakpoints.sm}px) {
+      font-size: 48px;
+    }
+  `
 );
 
 export const TextH1_64 = styled.h1(
-	({ theme: { fontSizes } }) => css`
-			${repeatableHeaderCss};
-			font-size: ${fontSizes[64]}px;
-	`,
+  ({ theme: { fontSizes } }) => css`
+    ${repeatableHeaderCss};
+    font-size: ${fontSizes[64]}px;
+
+    @media (max-width: ${breakpoints.sm}px), (max-height: ${breakpoints.sm}px) {
+      font-size: 48px;
+    }
+  `
 );
 
 export const TextH2_24 = styled.h2(
-	({ theme: { fontSizes } }) => css`
-			${repeatableHeaderCss};
-			font-size: ${fontSizes[24]}px;
-	`,
+  ({ theme: { fontSizes } }) => css`
+    ${repeatableHeaderCss};
+    font-size: ${fontSizes[24]}px;
+  `
 );
 
 export const TextH2_24_15 = styled.h2(
-	({ theme: { fontSizes } }) => css`
-			${repeatableHeaderCss};
-			font-size: ${fontSizes[24]}px;
-			letter-spacing: .15rem;
-	`,
+  ({ theme: { fontSizes } }) => css`
+    ${repeatableHeaderCss};
+    font-size: ${fontSizes[24]}px;
+    letter-spacing: 0.15rem;
+  `
 );
 
 export const TextH3_20 = styled.h3(
-	({ theme: { fontSizes } }) => css`
-			${repeatableHeaderCss};
-			font-size: ${fontSizes[20]}px;
-	`,
+  ({ theme: { fontSizes } }) => css`
+    ${repeatableHeaderCss};
+    font-size: ${fontSizes[20]}px;
+  `
 );
