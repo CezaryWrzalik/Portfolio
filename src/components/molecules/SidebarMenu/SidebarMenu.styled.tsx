eefe01fda06f521d81eebaa888f3e3ecc1751f32
CezaryWrzalik <wrzalikc@gmail.com>
2022-04-19 19:43:04 +0200

    (Fix): z-index menu

--- src/components/molecules/SidebarMenu/SidebarMenu.styled.tsx
+++ src/components/molecules/SidebarMenu/SidebarMenu.styled.tsx
@@ -33,6 +33,7 @@ export const SidebarMenuWrapper = styled.div(
     align-items: center;
     justify-content: center;
     padding: 0;
+    z-index: 3;
 
     @media (max-width: ${breakpoints.sm}px) {
       ${sidebarMenuMobileStyles};
